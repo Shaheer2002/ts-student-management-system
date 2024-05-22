e94b1edfe3151fd4eeb21cb972066d3aa5200e2e
Muhammad Shaheer <60762580+Shaheer2002@users.noreply.github.com>
2024-05-22 15:02:33 +1200

    Added Functionality

--- index.js
+++ index.js
@@ -1,40 +1,98 @@
 import inquirer from "inquirer";
 //initialising an empty array with student interface object
 let students = [];
-let courses = [{
-        name: 'BS',
+let courses = [
+    {
+        name: "BS",
         id: 0,
-        tutionFee: 1000
+        tutionFee: 1000,
     },
     {
-        name: 'MS',
+        name: "MS",
         id: 1,
-        tutionFee: 1500
+        tutionFee: 1500,
     },
     {
-        name: 'DAE',
+        name: "DAE",
         id: 2,
-        tutionFee: 2000
-    }];
+        tutionFee: 2000,
+    },
+];
 let isRunning = true;
 while (isRunning) {
     const answers = await inquirer.prompt([
         {
-            type: 'list',
-            name: 'action',
-            message: 'What do you want to do?',
-            choices: ['Add Student', 'View Students', 'Exit']
-        }
+            type: "list",
+            name: "action",
+            message: "What do you want to do?",
+            choices: ["Add Student", "View Students", "Manage Student", "Exit"]
+        },
     ]);
     switch (answers.action) {
-        case 'Add Student':
-            console.log('Add Student case');
+        case "Add Student":
+            console.log("Add Student case");
+            const newUserInput = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "Enter student name: ",
+                },
+                {
+                    type: "number",
+                    name: "balance",
+                    message: "Enter student balance: ",
+                },
+            ]);
+            students.push({
+                name: newUserInput.name,
+                id: 10000 + students.length,
+                balance: newUserInput.balance,
+                courses: []
+            });
+            console.log(newUserInput.name + " has been added.");
             break;
-        case 'View Students':
-            console.log('View Student case');
+        case "View Students":
+            //   console.table(students);
+            students.forEach((student) => {
+                console.log(student.name);
+            });
             break;
-        case 'Exit':
-            console.log('Exit case');
+        case 'Manage Student':
+            const choices = await inquirer.prompt([{
+                    type: 'list',
+                    name: 'action',
+                    message: 'What do you want to do?',
+                    choices: ['Enrol In A Course', 'Pay Fee', 'Update Balance', 'Exit']
+                }]);
+            switch (choices.action) {
+                case 'Enrol In A Course':
+                    console.log('Available Courses: ');
+                    console.table(courses);
+                    console.log('Available Students: ');
+                    console.table(students);
+                    const courseInput = await inquirer.prompt([{
+                            type: 'input',
+                            name: 'courseId',
+                            message: 'Enter course id?'
+                        }, {
+                            type: 'number',
+                            name: 'studentId',
+                            message: 'Enter student id?'
+                        }]);
+                    students[courseInput.studentId - 10000].courses.push(courses[courseInput.courseId]);
+                    console.log(students[courseInput.studentId - 10000].name + ' has been enrolled in ' + courses[courseInput.courseId].name);
+                    break;
+                case 'Pay Fee':
+                    break;
+                case 'Update Balance':
+                    break;
+                case 'Exit':
+                    break;
+                default:
+                    break;
+            }
+        case "Exit":
+            console.log("Exit case");
             isRunning = false;
             break;
         default:
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { table } from "console";
 import inquirer from "inquirer";
 
 // const Student = {
@@ -8,56 +9,119 @@ import inquirer from "inquirer";
 // }
 
 interface Student {
-    name: string,
-    id: number,
-    courses: string[],
-    balance: number,
+  name: string;
+  id: number;
+  courses: { name: string; id: number; tutionFee: number }[];
+  balance: number;
 }
 
 //initialising an empty array with student interface object
-let students: Student[] = []
+let students: Student[] = [];
 
-let courses: {name: string, id: number, tutionFee: number}[] = [{
-    name: 'BS',
+let courses: { name: string; id: number; tutionFee: number }[] = [
+  {
+    name: "BS",
     id: 0,
-    tutionFee: 1000
-},
-{
-    name: 'MS',
+    tutionFee: 1000,
+  },
+  {
+    name: "MS",
     id: 1,
-    tutionFee: 1500
-},
-{
-    name: 'DAE',
+    tutionFee: 1500,
+  },
+  {
+    name: "DAE",
     id: 2,
-    tutionFee: 2000
-}]
+    tutionFee: 2000,
+  },
+];
 
-let isRunning: boolean = true
-while(isRunning) {
-    const answers = await inquirer.prompt([
+let isRunning: boolean = true;
+while (isRunning) {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What do you want to do?",
+      choices: ["Add Student", "View Students", "Manage Student", "Exit"]
+    },
+  ]);
+
+  switch (answers.action) {
+    case "Add Student":
+      console.log("Add Student case");
+      const newUserInput = await inquirer.prompt([
         {
+          type: "input",
+          name: "name",
+          message: "Enter student name: ",
+        },
+        {
+          type: "number",
+          name: "balance",
+          message: "Enter student balance: ",
+        },
+      ]);
+      students.push({
+        name: newUserInput.name,
+        id: 10000 + students.length,
+        balance: newUserInput.balance,
+        courses: []
+      });
+      console.log(newUserInput.name + " has been added.");
+      break;
+
+    case "View Students":
+    //   console.table(students);
+    //   students.forEach((student) => {
+    //     console.log(student.name)
+    //   })
+      for (let student of students) {
+        console.log(student.name)
+      }
+      break;
+    
+    case 'Manage Student':
+        const choices = await inquirer.prompt([{
             type: 'list',
             name: 'action',
             message: 'What do you want to do?',
-            choices: ['Add Student', 'View Students', 'Exit']
+            choices: ['Enrol In A Course', 'Pay Fee', 'Update Balance', 'Exit']
+        }])
+        switch (choices.action) {
+            case 'Enrol In A Course':
+                console.log('Available Courses: ')
+                console.table(courses)
+                console.log('Available Students: ')
+                console.table(students)
+                const courseInput = await inquirer.prompt([{
+                    type: 'input',
+                    name: 'courseId',
+                    message: 'Enter course id?'
+                }, {
+                    type: 'number',
+                    name: 'studentId',
+                    message: 'Enter student id?'
+                }])
+                students[courseInput.studentId - 10000].courses.push(courses[courseInput.courseId])
+                console.log(students[courseInput.studentId - 10000].name + ' has been enrolled in '+ courses[courseInput.courseId].name)
+                break;
+            case 'Pay Fee':
+                break;
+            case 'Update Balance':
+                break;
+            case 'Exit':
+                break;
+            default:
+                break;
         }
-    ])
+    case "Exit":
+      console.log("Exit case");
+      isRunning = false;
+      break;
 
-    switch(answers.action) {
-        case 'Add Student':
-            console.log('Add Student case')
-            break;
-        case 'View Students':
-            console.log('View Student case')
-            break;
-        case 'Exit':
-            console.log('Exit case')
-            isRunning = false;
-            break;
-
-        default:
-            isRunning = false;
-        break;
-    }
+    default:
+      isRunning = false;
+      break;
+  }
 }
